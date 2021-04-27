2af21ea828a2847bd24a9a7425d501c6b4d3ee0b
TunamiRider <yoite.sy@gmail.com>
2021-04-27 12:11:01 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yuki Suzuki on 4/27/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
